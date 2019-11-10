8b58df571d70912a4588f36886c69ec44b400c05
unknown <elelion@yandex.ru>
2019-11-10 19:42:24 +0300

    Очередные правки, согласно замечаниям наставника

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 ﻿CREATE DATABASE task_force CHARACTER SET 'utf8';
 
--- **
+
+-- NOTE: task -----------------------------------------------------------------
 
 CREATE TABLE category (
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -71,8 +72,77 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 CREATE INDEX index_category ON category(id);
 
 ALTER TABLE task ADD FOREIGN KEY (rating_id) REFERENCES rating(id);
+
 ALTER TABLE comment ADD FOREIGN KEY (rating_id) REFERENCES rating(id);
 ALTER TABLE comment ADD FOREIGN KEY (task_id) REFERENCES task(id);
 ALTER TABLE comment ADD FOREIGN KEY (ststus_id) REFERENCES status(id);
+
 ALTER TABLE task ADD FOREIGN KEY (status_id) REFERENCES status(id);
+
 ALTER TABLE task_image ADD FOREIGN KEY (task_id) REFERENCES task(id);
+
+
+-- NOTE: user -----------------------------------------------------------------
+
+CREATE TABLE account (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(64) NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	password CHAR(64) NOT NULL,
+	address TEXT NOT NULL,
+	born DATETIME NOT NULL DEFAULT NOW(),
+	about TEXT,
+	phone INT NOT NULL,
+	skype VARCHAR(64) NOT NULL,
+	messanger VARCHAR(64),
+	visit DATETIME NOT NULL DEFAULT NOW(),
+	quest_completed INT(4),
+
+	city_id INT,
+	specialization_id INT,
+	notification_id INT
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE city (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(32) NOT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE user_image (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	image_path VARCHAR(45) NOT NULL,
+	account_id INT
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+
+CREATE TABLE specialization (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(32)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE notification (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(32)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE message (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	message TEXT,
+	account_id INT
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- **
+
+ALTER TABLE message ADD FOREIGN KEY (account_id) REFERENCES account(id);
+
+ALTER TABLE account ADD FOREIGN KEY (specialization_id) REFERENCES specialization(id);
+ALTER TABLE account ADD FOREIGN KEY (notification_id) REFERENCES notification(id);
+ALTER TABLE account ADD FOREIGN KEY (city_id) REFERENCES city(id);
+
+ALTER TABLE user_image ADD FOREIGN KEY (account_id) REFERENCES account(id);
