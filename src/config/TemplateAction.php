ba4207fdca2f08a5974bec8ce3fb556467516af2
unknown <elelion@yandex.ru>
2019-11-23 19:45:11 +0300

    Правки по замечанию наставника

--- src/config/TemplateAction.php
+++ src/config/TemplateAction.php
@@ -3,9 +3,23 @@ declare(strict_types=1);
 
 namespace config;
 
-abstract class TemplateAction
+abstract class Action
 {
   abstract public static function getTitle(): string;
-  abstract public static function getAction(): string;
+
+  // **
+
+  private static function getClass()
+  {
+    return __CLASS__;
+  }
+
+  public static function getAction(): string
+  {
+    static::getClass();
+  }
+
+  // **
+
   abstract public static function rightsVerification($rightsVerification): bool;
 }
