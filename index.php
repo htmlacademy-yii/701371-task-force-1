b0bc73bccbe83076e0cfd43ad3c1a585c76e0c92
unknown <elelion@yandex.ru>
2019-12-05 18:05:09 +0300

    added checks for a new class

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ use app\components\Task;
 use app\components\Action;
 use app\components\AvailableActions;
 
+use app\components\import\CsvToSqlConverter;
+
 use app\components\RespondAction;
 use app\components\CancelAction;
 use app\components\CompleteAction;
@@ -22,15 +24,27 @@ try {
 		'clientId' => 5,
 		'completed' => '2019-12-24'
 	]);
-
-	// $myTask = new Task('foo');
-	// $myTask = new Task('STATUS_NEW');
 } catch (Throwable $exception) {
 	error_log($exception->getMessage());
 }
+
+/**/
 
 echo PHP_EOL;
 var_dump(AvailableActions::getNextStatus(new FailAction));
 
 $task = new Task;
 var_dump($task->getCurrentIdClient());
+
+/**/
+
+echo PHP_EOL;
+try {
+	$import = new CsvToSqlConverter;
+	$import->setFileName('categories.csv');
+	$import->beginEvent();
+} catch (Throwable $exception) {
+	error_log($exception->getMessage());
+}
+// $sql = CsvToSqlConverter::getSqlFromCsv('data/cities.csv', $value_map, 'city');
+// CsvToSqlConverter::writeInSqlFile('data/sql/city.sql', $sql);
