b12d0f8986f9eef7b1ee2063b275c103bcab8b33
unknown <elelion@yandex.ru>
2019-12-11 15:01:19 +0300

    The first step on a big way

--- src/components/CancelAction.php
+++ src/components/CancelAction.php
@@ -5,14 +5,14 @@ namespace app\components;
 
 class CancelAction extends Action
 {
-  public static function rightsVerification(Task $task, int $userId): bool
-  {
-    return $task->getCurrentStatus() === Task::STATUS_NEW
-      && $task->getCurrentIdClient() === $userId;
-  }
+	public static function rightsVerification(Task $task, int $userId): bool
+	{
+		return $task->getCurrentStatus() === Task::STATUS_NEW
+			&& $task->getCurrentIdClient() === $userId;
+	}
 
-  public static function getTitle(): string
-  {
-    return 'Отменить задание';
-  }
+	public static function getTitle(): string
+	{
+		return 'Отменить задание';
+	}
 }
--- src/components/RespondAction.php
+++ src/components/RespondAction.php
@@ -5,14 +5,14 @@ namespace app\components;
 
 class RespondAction extends Action
 {
-  public static function rightsVerification(Task $task, int $userId): bool
-  {
-    return $task->getCurrentStatus() === Task::STATUS_NEW
-      && $task->getIdExecutor() === $userId;
-  }
+	public static function rightsVerification(Task $task, int $userId): bool
+	{
+		return $task->getCurrentStatus() === Task::STATUS_NEW
+			&& $task->getIdExecutor() === $userId;
+	}
 
-  public static function getTitle(): string
-  {
-    return 'Откликнуться';
-  }
+	public static function getTitle(): string
+	{
+		return 'Откликнуться';
+	}
 }
