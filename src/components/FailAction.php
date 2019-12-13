8c4ff38c8cd09fe03ab06bcf0d7a7a945104ef68
unknown <elelion@yandex.ru>
2019-12-13 15:29:41 +0300

    transferring our previously written classes

---
+++ src/components/FailAction.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace app\components;
+
+class FailAction extends Action
+{
+	public static function rightsVerification(Task $task, int $userId): bool
+	{
+		return $task->getCurrentStatus() === Task::STATUS_PROGRESS
+			&& $task->getIdExecutor() === $userId;
+	}
+
+	public static function getTitle(): string
+	{
+		return 'Отказаться';
+	}
+}
