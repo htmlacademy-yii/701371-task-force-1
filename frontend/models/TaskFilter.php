f2f461111d7086546310f354f6d7fa75748fc495
Elelion <elelion@yandex.ru>
2020-01-19 19:35:01 +0300

    supplemented model

--- frontend/models/TaskFilter.php
+++ frontend/models/TaskFilter.php
@@ -2,12 +2,11 @@
 
 namespace frontend\models;
 
-use Yii;
-use yii\db\ActiveRecord;
+use yii\base\Model;
 use yii\db\ActiveQuery;
+use yii\db\Expression;
 
-
-class TaskFilter extends ActiveRecord
+class TaskFilter extends Model
 {
     public $categories = [];
     public $myCity = null;
@@ -16,36 +15,68 @@ class TaskFilter extends ActiveRecord
     public $period = 'all';
     public $search = null;
 
-    public function applyFilters(ActiveQuery $tasks): ActiveQuery
+    public function applyFilters(ActiveQuery &$tasks): void
     {
         if ($this->categories) {
             $tasks->andWhere(['category_id' => $this->categories]);
         }
+
+        if ($this->myCity) {
+            $tasks->andWhere(['city_id' => $this->myCity]);
+        }
+
+        // TODO: ???
+        if ($this->withoutExecutor) {
+            $tasks->andWhere(['city_id' => $this->withoutExecutor]);
+        }
+
+        if ($this->remoteWork) {
+            $tasks->andWhere(['executor_id' => $this->remoteWork]);
+        }
+
+        switch ($this->period) {
+            case 'day':
+                $tasks->andWhere(['>', 'created', new Expression(
+                        'CURRENT_TIMESTAMP() - INTERVAL 1 DAY')]);
+                break;
+
+            case 'week':
+                $tasks->andWhere(['>', 'created', new Expression(
+                    'CURRENT_TIMESTAMP() - INTERVAL 7 DAY')]);
+                break;
+
+            case 'month':
+                $tasks->andWhere(['>', 'created', new Expression(
+                    'CURRENT_TIMESTAMP() - INTERVAL 30 DAY')]);
+                break;
+        }
+
+        if ($this->search) {
+            $tasks->andWhere("MATCH(task.description, task.title) 
+                AGAINST ('$this->search')");
+        }
     }
 
-    public function attributeLabels()
+    public function attributeLabels(): array
     {
         return [
-            'categories' => 'Категории',
-            'myCity' => 'Мой город',
-            'withoutExecutor' => 'Без исполнителя',
-            'remoteWork' => 'Удаленная работа',
-            'period' => 'Дата',
-            'search' => 'Поиск по названию',
+            'categories'        => 'Категории',
+            'myCity'            => 'Мой город',
+            'withoutExecutor'   => 'Без исполнителя',
+            'remoteWork'        => 'Удаленная работа',
+            'period'            => 'Дата',
+            'search'            => 'Поиск по названию',
         ];
     }
 
-    public function rules()
+    public function rules(): array
     {
         return [
-            [[
-                'categories',
-                'myCity',
-                'withoutExecutor',
-                'remoteWork',
-                'date',
-                'find'
-            ], 'safe']
+            [['categories', 'myCity', 'withoutExecutor', 'remoteWork', 'date',
+                'find'], 'safe'],
+
+            [['myCity', 'withoutExecutor', 'remoteWork'], 'boolean'],
+            [['find'], 'string']
         ];
     }
 }
