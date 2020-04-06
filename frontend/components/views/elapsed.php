e19e94725d5065824b63ca8b81e25b9f2715b5de
Elelion <elelion@yandex.ru>
2020-04-06 08:26:21 +0300

    corrected the date output

--- frontend/components/views/elapsed.php
+++ frontend/components/views/elapsed.php
@@ -4,6 +4,20 @@ use frontend\helpers\PluralFormHelper;
 /** @var DateInterval $interval */
 ?>
 
+<?php if ((int) $interval->y >= 1): ?>
+    <?= $interval->y; ?>
+    <?= PluralFormHelper::format(
+        $interval->y, 'год', 'года', 'лет'
+    ); ?>
+<?php endif; ?>
+
+<?php if ((int) $interval->m >= 1): ?>
+    <?= $interval->m; ?>
+    <?= PluralFormHelper::format(
+        $interval->m, 'месяц', 'месяца', 'месяцев'
+    ); ?>
+<?php endif; ?>
+
 <?php if ((int) $interval->d >= 1): ?>
     <?= $interval->d; ?>
     <?= PluralFormHelper::format(
@@ -13,5 +27,5 @@ use frontend\helpers\PluralFormHelper;
 
 <?= $interval->h; ?>
 <?= PluralFormHelper::format(
-    $interval->h, 'час', 'часа', 'часов'
+    $interval->h, ' час', ' часа', ' часов'
 ); ?>
