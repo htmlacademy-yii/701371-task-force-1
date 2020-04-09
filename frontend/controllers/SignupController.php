d4424fdc857673d31155ae03c7cbccb9c28073b6
Elelion <elelion@yandex.ru>
2020-04-09 23:41:00 +0300

    filling in the controller

--- frontend/controllers/SignupController.php
+++ frontend/controllers/SignupController.php
@@ -3,13 +3,26 @@
 
 namespace frontend\controllers;
 
-
 use yii\web\Controller;
+use frontend\models\UserSignupForm;
+use frontend\models\City;
 
 class SignupController extends Controller
 {
     public function actionIndex(): string
     {
-        return $this->render('index');
+        $user = new UserSignupForm();
+        $cities = City::find()->orderBy(['title' => SORT_ASC])->all();
+
+        if (Yii::$app->request->getIsPost()) {
+            $user->load(Yii::$app->request->post());
+            if (!$user->validate()) {
+                $errors = $user->getErrors();
+                var_dump($errors);
+                die;
+            }
+        }
+
+        return $this->render('index', compact('user'));
     }
 }
