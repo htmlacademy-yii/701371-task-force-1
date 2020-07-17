5ea8ecd95f90f5f6fe2764a7d0c1f462dbb788b9
Alexander Gints <elelion@Mac-mini.local>
2020-07-18 00:34:21 +0300

    restoring after crashing & completed task 7.3

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,10 +16,11 @@ class AppAsset extends AssetBundle
         'css/style.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-        // 'yii\bootstrap\BootstrapPluginAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
