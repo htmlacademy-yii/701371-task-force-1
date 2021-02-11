53f943ebee16d79a0e05757741b823c59cc643b3
elelion <alexander.gints@icloud.com>
2021-02-11 07:10:26 +0300

    del not used method

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -100,63 +100,6 @@ class SiteController extends Controller
         return $this->goHome();
     }
 
-    public function onAuthSuccess_(OAuth2 $client)
-    {
-        $attributes = $client->getUserAttributes();
-
-        /* @var $auth Auth */
-        $auth = Auth::find()->where([
-            'source' => $client->getId(),
-            'source_id' => $attributes['id'],
-        ])->one();
-
-        if (Yii::$app->user->isGuest) {
-            if ($auth) { // авторизация
-                $user = $auth->user;
-                Yii::$app->user->login($user);
-            } else { // регистрация
-                if (isset($attributes['email']) && Users::find()->where(['email' => $attributes['email']])->exists()) {
-                    Yii::$app->getSession()->setFlash('error', [
-                        Yii::t('app', "Пользователь с такой электронной почтой как в {client} уже существует, но с ним не связан. Для начала войдите на сайт использую электронную почту, для того, что бы связать её.", ['client' => $client->getTitle()]),
-                    ]);
-                } else {
-                    $user = new Users([
-                        'name' => $attributes['first_name'] . ' ' . $attributes['last_name'],
-                        'email' => $attributes['email'],
-                        'password' => Yii::$app->security->generateRandomString(6),
-                    ]);
-                    $user->generateAuthKey();
-                    $user->generatePasswordResetToken();
-                    $transaction = $user->getDb()->beginTransaction();
-                    if ($user->save()) {
-                        $auth = new Auth([
-                            'user_id' => $user->id,
-                            'source' => $client->getId(),
-                            'source_id' => (string)$attributes['id'],
-                        ]);
-                        if ($auth->save()) {
-                            $transaction->commit();
-                            Yii::$app->user->login($user);
-                        } else {
-                            print_r($auth->getErrors());
-                        }
-                    } else {
-                        print_r($user->getErrors());
-                    }
-                }
-            }
-        } else { // Пользователь уже зарегистрирован
-            if (!$auth) { // добавляем внешний сервис аутентификации
-                $auth = new Auth([
-                    'user_id' => Yii::$app->user->id,
-                    'source' => $client->getId(),
-                    'source_id' => $attributes['id'],
-                ]);
-                $auth->save();
-            }
-        }
-    }
-
     /**
      * Displays homepage.
      *
