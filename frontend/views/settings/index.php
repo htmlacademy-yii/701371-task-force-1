e467756cba44bfce8e922c115b03092c884d0dae
elelion <alexander.gints@icloud.com>
2021-03-04 18:33:45 +0300

    fix errors

--- frontend/views/settings/index.php
+++ frontend/views/settings/index.php
@@ -33,7 +33,7 @@ SettingsFormAsset::register($this);
                     <div class="account__redaction-section-wrapper">
                         <div class="account__redaction-avatar">
 
-                            <?= Html::img('@web/img/' . Yii::$app->user->identity->userAvatarPath(),
+                            <?= Html::img('@web/img/' . Yii::$app->user->identity->getUserAvatarPath(),
                                 [
                                     'alt' => 'Аватар пользователя',
                                     'style' => 'width: 142px; height: 142px; border-radius: 4px;'
